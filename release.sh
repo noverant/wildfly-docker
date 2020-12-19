24a57bcc60662d4fc7b50b754a00639146715ba0
Jonathan Putney <jputney@noverant.com>
2020-12-19 13:49:36 -0500

    21.0.2 Upgrade

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-WILDFLY_RELEASE=19.1.0
+WILDFLY_RELEASE=21.0.2
 MARIADB_VERSION=`mvn -f mariadb/pom.xml help:evaluate -Dexpression=version.mariadb -q -DforceStdout`
 MYSQL_VERSION=`mvn -f mysql/pom.xml help:evaluate -Dexpression=version.mysql -q -DforceStdout`
 
