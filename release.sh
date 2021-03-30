f088c060067f17e33d25f04c3267f419d0c10350
Jonathan Putney <jputney@noverant.com>
2021-03-30 14:44:51 -0400

    Updates

--- release.sh
+++ release.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-WILDFLY_RELEASE=22.0.1
+WILDFLY_RELEASE=23.0.0
 MARIADB_VERSION=`mvn -f mariadb/pom.xml help:evaluate -Dexpression=version.mariadb -q -DforceStdout`
 MYSQL_VERSION=`mvn -f mysql/pom.xml help:evaluate -Dexpression=version.mysql -q -DforceStdout`
 
 echo "=> MariaDB driver: ${MARIADB_VERSION}"
 echo "=> MySQL driver: ${MYSQL_VERSION}"
 
-docker build . -t noverant/wildfly:latest -t noverant/wildfly:$WILDFLY_RELEASE
+docker build . --no-cache -t noverant/wildfly:latest -t noverant/wildfly:$WILDFLY_RELEASE
 docker push noverant/wildfly:latest
 docker push noverant/wildfly:$WILDFLY_RELEASE
 
-docker build mariadb/ -t noverant/wildfly:latest-mariadb -t noverant/wildfly:$WILDFLY_RELEASE-mariadb -t noverant/wildfly:$WILDFLY_RELEASE-mariadb-$MARIADB_VERSION
+docker build mariadb/ --no-cache -t noverant/wildfly:latest-mariadb -t noverant/wildfly:$WILDFLY_RELEASE-mariadb -t noverant/wildfly:$WILDFLY_RELEASE-mariadb-$MARIADB_VERSION
 docker push noverant/wildfly:latest-mariadb
 docker push noverant/wildfly:$WILDFLY_RELEASE-mariadb
 docker push noverant/wildfly:$WILDFLY_RELEASE-mariadb-$MARIADB_VERSION
 
-docker build mysql/ -t noverant/wildfly:latest-mysql -t noverant/wildfly:$WILDFLY_RELEASE-mysql -t noverant/wildfly:$WILDFLY_RELEASE-mysql-$MYSQL_VERSION
+docker build mysql/ --no-cache -t noverant/wildfly:latest-mysql -t noverant/wildfly:$WILDFLY_RELEASE-mysql -t noverant/wildfly:$WILDFLY_RELEASE-mysql-$MYSQL_VERSION
 docker push noverant/wildfly:latest-mysql
 docker push noverant/wildfly:$WILDFLY_RELEASE-mysql
 docker push noverant/wildfly:$WILDFLY_RELEASE-mysql-$MYSQL_VERSION
