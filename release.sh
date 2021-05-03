012c39d1609c5ca38a5dda917e93cbe27c6ab3f8
Jonathan Putney <jputney@noverant.com>
2021-05-03 08:58:01 -0400

    Wildfly 23.0.2

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-WILDFLY_RELEASE=23.0.1
+WILDFLY_RELEASE=23.0.2
 MARIADB_VERSION=`mvn -f mariadb/pom.xml help:evaluate -Dexpression=version.mariadb -q -DforceStdout`
 MYSQL_VERSION=`mvn -f mysql/pom.xml help:evaluate -Dexpression=version.mysql -q -DforceStdout`
 
